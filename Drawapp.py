6b26317d5a0eba935571f9ad8f9b314f1ae624a4
Marius123799 <80588132+Marius123799@users.noreply.github.com>
2021-03-27 17:35:22 +0200

    Update Drawapp.py

--- Drawapp.py
+++ Drawapp.py
@@ -1,72 +1,120 @@
-# import turtle library
-import turtle  
-print("1 - Poligon 2- Patrat in patrat 3-spirale")
-Draw = int(input("Desen "))
+name=input("Nume: ")
+import random
+viata=100
+power=random.randrange(1,100)
+inam= 150
+m=random.randrange(1,20)
 
-colorbg = input("Background color: ")
-pencol = input("pen color: ")
-title = input("Titlu: ")
-wn = turtle.Screen()
-pen = turtle.Turtle()
-pen.color(pencol)
-wn.bgcolor(colorbg)
-wn.title(title)
-if (Draw == 1):
-    SIDES = int(input("Laturi "))
-    LENGTH = int(input("Lungime "))
-    if(LENGTH > 200):
-        print("Va recomandam mai valori mai mici de 200 la Lungime")
-    if(SIDES == 4):
-        print("Nu putem desena un patrat cand cerinta este de poligon")
+print("Ce vrei sa faci?")
+print("Lobveste - 1")
+print("Adauga viata - 2")
+ch=int(input("Varianta: "))
+if (ch ==1):
+    print(name,"-",m)
+    if(viata == 0):
+        print("Offf")
     else:
-  
-
-        polygon = turtle.Turtle()
-        num_sides = SIDES
-        side_length = LENGTH
-        angle = 360.0 / num_sides
-        for i in range(num_sides):
-            polygon.forward(side_length)           
-            polygon.right(angle) 
-        turtle.done()
-        print("DONE!")
-
-if (Draw == 2):
-
-   
-    import turtle             
-
-
-    # import turtle library
-    import turtle             
-    wn = turtle.Screen()
-
-    pen = turtle.Turtle()
-    pen.color(pencol)
-    def sqrfunc(size):
-        for i in range(4):
-            pen.fd(size)
-            pen.left(90)
-            size = size - 5
-    sqrfunc(146)
-    sqrfunc(126)
-    sqrfunc(106)
-    sqrfunc(86)
-    sqrfunc(66)
-    sqrfunc(46)
-    sqrfunc(26)
-    print("DONE!")
+        print("Bot turn")
+        viata=viata-m
+        if viata == 90:
+             print("Ai viata egala cu  90 ",viata)
+        if viata <= 60 and viata<=40:
+             print("Ai viata <  60 ",viata)
+        if viata <= 40:
+             print("Ai viata ",viata)
+        if(viata != 0):
+             print(viata,"viata")
+        print("your turn")
+        power=random.randrange(1,100)
+        inam=inam-power
+        print("-",inam)
+else:
+     if (ch ==2):
+          print("Viata",viata,"+","20")
+          vr=viata+20
+          if (vr>100):
+               print("Ai viata 100!")
+               viata=vr-100+viata
+               viata=viata+20
 
 
-if(Draw == 3 ):
-    import turtle             
-    colors = [ "red","purple","blue","green","orange","yellow"]
-    pen = turtle.Pen()
 
-    for x in range(360):
-        pen.pencolor(colors[x % 6])
-        pen.width(x/100 + 1)
-        pen.forward(x)
-        pen.left(59)
+if(inam == 0):
+     print("Yay")
+print("Ce vrei sa faci?")
+print("Lobveste - 1")
+print("Adauga viata - 2")
+ch=int(input("Varianta: "))
+if (ch ==1):
+    print(name,"-",m)
+    if(viata == 0):
+        print("Offf")
+    else:
+        print("Bot turn")
+        viata=viata-m
+        if viata == 90:
+             print("Ai viata egala cu  90 ",viata)
+        if viata <= 60 and viata<=40:
+             print("Ai viata <  60 ",viata)
+        if viata <= 40:
+             print("Ai viata ",viata)
+        if(viata != 0):
+             print(viata,"viata")
+        print("your turn")
+        power=random.randrange(1,100)
+        inam=inam-power
+        print("-",inam)
+else:
+     if (ch ==2):
+          print("Viata",viata,"+","20")
+          vr=viata+20
+          if (vr>100):
+               print("Ai viata 100!")
+               viata=vr-100+viata
+               viata=viata+20
 
-   
+
+
+if(inam == 0):
+     print("Yay")
+print("Ce vrei sa faci?")
+print("Lobveste - 1")
+print("Adauga viata - 2")
+ch=int(input("Varianta: "))
+if (ch ==1):
+    print(name,"-",m)
+    if(viata == 0):
+        print("Offf")
+    else:
+        print("Bot turn")
+        viata=viata-m
+        if viata == 90:
+             print("Ai viata egala cu  90 ",viata)
+        if viata <= 60 and viata<=40:
+             print("Ai viata <  60 ",viata)
+        if viata <= 40:
+             print("Ai viata ",viata)
+        if(viata != 0):
+             print(viata,"viata")
+        print("your turn")
+        power=random.randrange(1,100)
+        inam=inam-power
+        print("-",inam)
+        print(viata)
+else:
+     if (ch ==2):
+          print("Viata",viata,"+","20")
+          vr=viata+20
+          if (vr>100):
+               print("Ai viata 100!")
+               viata=vr-100+viata
+               viata=viata+20
+
+
+
+if(inam == 0):
+     print("Yay")
+          
+
+
+
